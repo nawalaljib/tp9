e4fb460cfee31313e31b7d870d58655ba6d3cdc1
nawal <nawal.aljib2019@gmail.com>
2024-11-16 21:18:11 +0100

    first commit

---
+++ src/test/java/ma/ac/uir/AppTest.java
@@ -0,0 +1,19 @@
+package ma.ac.uir;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
